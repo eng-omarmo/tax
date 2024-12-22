806c93771560bdd115a5784df177a96ae0b67512
eng-omarmoo <omarmoo683@gmail.com>
2024-12-22 14:56:22 +0300

    property changes

--- app/Http/Controllers/propertyController.php
+++ app/Http/Controllers/propertyController.php
@@ -16,22 +16,22 @@ class propertyController extends Controller
         $monitoringStatuses = Property::pluck('monitoring_status')->unique();
         //app filter
         $query  = Property::query();
-        if($request->filled('search')){
+        if ($request->filled('search')) {
             $query->where('property_name', 'like', '%' . $request->search . '%')
-            ->orWhere('property_phone', 'like', '%' . $request->search . '%')
-            ->orWhere('nbr', 'like', '%' . $request->search . '%')
-            ->orWhere('house_code', 'like', '%' . $request->search . '%')
-            ->orWhere('tenant_name', 'like', '%' . $request->search . '%')
-            ->orWhere('tenant_phone', 'like', '%' . $request->search . '%')
-            ->orWhere('branch', 'like', '%' . $request->search . '%')
-            ->orWhere('zone', 'like', '%' . $request->search . '%')
-            ->orWhere('house_type', 'like', '%' . $request->search . '%')
-            ->orWhere('status', 'like', '%' . $request->search . '%');
+                ->orWhere('property_phone', 'like', '%' . $request->search . '%')
+                ->orWhere('nbr', 'like', '%' . $request->search . '%')
+                ->orWhere('house_code', 'like', '%' . $request->search . '%')
+                ->orWhere('tenant_name', 'like', '%' . $request->search . '%')
+                ->orWhere('tenant_phone', 'like', '%' . $request->search . '%')
+                ->orWhere('branch', 'like', '%' . $request->search . '%')
+                ->orWhere('zone', 'like', '%' . $request->search . '%')
+                ->orWhere('house_type', 'like', '%' . $request->search . '%')
+                ->orWhere('status', 'like', '%' . $request->search . '%');
         }
-        if($request->filled('status')){
+        if ($request->filled('status')) {
             $query->where('status', $request->status);
         }
-        if($request->filled('monetering_status')){
+        if ($request->filled('monetering_status')) {
             $query->where('monitoring_status', $request->monetering_status);
         }
         $properties = $query->paginate(10);
@@ -53,9 +53,9 @@ class propertyController extends Controller
             'tenant_name' => 'nullable|string|max:255',
             'tenant_phone' => 'nullable|string|max:15',
             'branch' => 'nullable|string|max:255',
-            'quarterly_tax_fee'=>'nullable|numeric',
-             'yearly_tax_fee'=>'nullable|numeric',
-             'dalal_company_name' => 'nullable|string|max:255',
+            'quarterly_tax_fee' => 'nullable|numeric',
+            'yearly_tax_fee' => 'nullable|numeric',
+            'dalal_company_name' => 'nullable|string|max:255',
             'zone' => 'nullable|string|max:255',
             'house_type' => 'nullable|string|max:255',
             'house_rent' => 'nullable|numeric',
@@ -68,13 +68,32 @@ class propertyController extends Controller
         if ($checkProperty) {
             return back()->with('error', 'Property name already exists.');
         }
-        Property::create($request->all());
+        // Create a new property
+        Property::create([
+            'property_name' => $request->property_name,
+            'property_phone' => $request->property_phone,
+            'nbr' => $request->nbr,
+            'house_code' => $request->house_code,
+            'tenant_name' => $request->tenant_name,
+            'tenant_phone' => $request->tenant_phone,
+            'branch' => $request->branch,
+            'zone' => $request->zone,
+            'house_type' => $request->house_type,
+            'house_rent' => $request->house_rent,
+            'quarterly_tax_fee' => $request->quarterly_tax_fee,
+            'yearly_tax_fee' => $request->yearly_tax_fee,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude,
+            'dalal_company_name' => $request->dalal_company_name,
+            'monitoring_status' => $request->monitoring_status,
+            'status' => $request->status,
+        ]);
         return redirect()->route('property.index')->with('success', 'Property registered successfully.');
     }
 
     public function edit($id)
     {
-       $property = Property::findorFail($id);
+        $property = Property::findorFail($id);
 
         return view('property.edit', compact('property'));
     }
