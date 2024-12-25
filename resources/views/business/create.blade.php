cb331e47e2e4dd7ac7b3339b18ab2364570ff3e0
eng-omarmoo <omarmoo683@gmail.com>
2024-12-25 14:48:43 +0300

    changes

--- resources/views/business/create.blade.php
+++ resources/views/business/create.blade.php
@@ -40,16 +40,16 @@
                                 </div>
                             @endif
 
-                            <form action="{{ route('business.update', $business->id) }}" method="POST">
+                            <form action="{{ route('business.store') }}" method="POST">
                                 @csrf
-                                @method('PUT')
+
 
                                 <div class="mb-20">
                                     <label for="name"
                                         class="form-label fw-semibold text-primary-light text-sm mb-8">Business Name <span
                                             class="text-danger-600">*</span></label>
                                     <input type="text" class="form-control radius-8" id="name" name="name"
-                                        placeholder="Enter Business Name" value="{{ old('name', $business->name) }}">
+                                        placeholder="Enter Business Name"
                                 </div>
 
                                 <div class="mb-20">
@@ -57,20 +57,20 @@
                                         class="form-label fw-semibold text-primary-light text-sm mb-8">Business Phone <span
                                             class="text-danger-600">*</span></label>
                                     <input type="text" class="form-control radius-8" id="phone" name="phone"
-                                        placeholder="Enter Phone" value="{{ old('phone', $business->phone) }}">
+                                        placeholder="Enter Phone"
                                 </div>
 
                                 <div class="mb-20">
                                     <label for="address"
                                         class="form-label fw-semibold text-primary-light text-sm mb-8">Business Address <span class="text-danger-600">*</span></label>
                                     <input type="text" class="form-control radius-8" id="address" name="address"
-                                        placeholder="Enter Business Address" value="{{ old('address', $business->address) }}">
+                                        placeholder="Enter Business Address" >
                                 </div>
 
                                 <div class="d-flex align-items-center justify-content-center gap-3">
                                     <a href="{{ route('business.index') }}" class="border border-danger-600 bg-hover-danger-200 text-danger-600 text-md px-56 py-11 radius-8">Cancel</a>
                                     <button type="submit"
-                                        class="btn btn-primary border border-primary-600 text-md px-56 py-12 radius-8">Update</button>
+                                        class="btn btn-primary border border-primary-600 text-md px-56 py-12 radius-8">Save </button>
                                 </div>
                             </form>
                         </div>
